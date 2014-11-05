c0efe3287d5567054a3f7ddbeec5f6f2e4b46414
Michele Pratusevich <mprat@mit.edu>
2014-11-04 23:01:33 -0500

    exercise 1 finished

--- python/paper_graphs.py
+++ python/paper_graphs.py
@@ -53,20 +53,20 @@ def margin_with_c():
 
 def support_vectors():
 	c = np.array([0.01, 0.1, 1, 10, 100])
-	small_overlap_linear = np.array([1.79, .93, .57, .5611, .5613])
-	big_overlap_linear = np.array([2.38, 1.93, 1.88, 1.88, 1.88])
-	ls_linear = np.array([.939, .48, .31, .23, .23])
-	nonSep2_linear = np.array([3.98, 3.37, 3.29, 3.29, 3.29])
+	small_overlap_linear = np.array([70, 37, 24, 22, 23])
+	big_overlap_linear = np.array([148, 131, 128, 128, 129])
+	ls_linear = np.array([177, 56, 13, 3, 31])
+	nonSep2_linear = np.array([399, 393, 392, 392, 393])
 
 	# small_overlap_gaussian_0001 = np.array([0.76, 0.076, 0.007, 0.0007, 7e-5])
 	# small_overlap_gaussian_01 = np.array([0.76, 0.076, 0.007, 0.0007, 7e-5])
 
-	stdev1_beta_0_5 = np.array([0.089, 0.0089, 8e-4, 8e-5, 8e-6])
-	stdev1_beta_0_0001 = np.array([8.9e-2, 8.9e-3, 8.9e-4, 8.9e-5, 8.9e-6])
-	stdev1_beta_1 = np.array([0.089, 0.0089, 8e-4, 8e-5, 8e-6])
+	# stdev1_beta_0_0001 = np.array([])
+	# stdev1_beta_0_5 = np.array([400, 400, 400, 400, 400])
+	smalloverlap_beta_1 = np.array([100, 100, 100, 100, 100])
 
-	nonSep2_beta_0_001 = np.array([3, 0.3, 3e-2, 3e-3, 3e-4])
-	nonSep2_beta_0_01 = np.array([0.3, 3e-2, 3e-3, 3e-4, 3e-5])
+	bigoverlap_beta_0_001 = np.array([200, 200, 200, 200, 200])
+	# nonSep2_beta_0_01 = np.array([400, 400, 400, 400, 400])
 
 
 	fig = plt.figure()
@@ -76,19 +76,18 @@ def support_vectors():
 	ax.plot(c, big_overlap_linear, label="bigOverlap Linear Kernel")
 	ax.plot(c, ls_linear, label="ls Linear Kernel")
 	ax.plot(c, nonSep2_linear, label="nonSep2 Linear Kernel")
-	ax.plot(c, stdev1_beta_0_0001, label=r"stdev1 $\beta = 0.001$")
-	ax.plot(c, stdev1_beta_0_5, label=r"stdev1 $\beta = 0.5$")
-	ax.plot(c, stdev1_beta_1, label=r"stdev1 $\beta = 1$")
-	ax.plot(c, nonSep2_beta_0_001,label=r"nonSep2 $\beta = 0.001$")
-	ax.plot(c, nonSep2_beta_0_01,label=r"nonSep2 $\beta = 0.01$")
+	# ax.plot(c, stdev1_beta_0_5, label=r"stdev1 $\beta = 0.5$")
+	ax.plot(c, smalloverlap_beta_1, label=r"stdev1 $\beta = 1$")
+	ax.plot(c, bigoverlap_beta_0_001,label=r"nonSep2 $\beta = 0.001$")
+	# ax.plot(c, nonSep2_beta_0_01,label=r"nonSep2 $\beta = 0.01$")
 
 	ax.set_xlabel('c')
 	ax.set_ylabel('Support vectors')
-	ax.set_xscale('linear')
+	ax.set_xscale('log')
 	ax.set_yscale('linear')
 	ax.set_xticks([])
 	ax.set_yticks([])
-	ax.set_ylim([0, 5])
+	ax.set_ylim([0, 420])
 
 	ax.legend()
 
